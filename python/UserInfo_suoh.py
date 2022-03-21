8c619db37d4d8b27f9d7258b3be78e55396ed44f
sungbinoh <wwera6421@gmail.com>
2022-03-21 13:57:06 +0900

    Deployed e77914b with MkDocs version: 1.2.3

--- python/UserInfo_suoh.py
+++
@@ -1,8 +0,0 @@
-UserInfo = {
-
-  'PDStudyLogEmail' : 'sungbin.oh@cern.ch',
-  'PDStudyLogWeb' : '', # leave if blank if you don't have webpage
-  'PDStudyLogWebDir' : '', # leave if blank if you don't have webpage
-  'LogEvery' : 1000,
-
-}
